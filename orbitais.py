dfe96b9badfeed2b7c7047a03af3832dc580ebef
IranFS <iran.f.dasilva@gmail.com>
2021-11-01 22:39:28 -0300

    1368pind_opt.mo228a.cube 	1368pind_td.xyz 	228.png 	coords.png

--- orbitais.py
+++ orbitais.py
@@ -30,13 +30,25 @@ pymol.pymol_argv = ['pymol','-qc'] + sys.argv[1:]
 pymol.finish_launching()
 cmd = pymol.cmd
 
+
 # acquire the xyz file and the cube files inside the folder
 # note that the xyz file must be passed as an argument
-
 coords = sys.argv[1]
 file_om=[f for f in glob("*.cube")]
-print(file_om)
-Oms = len(file_om)
+
+#	Manually enter this array of values, you should open the same xyz file that is 
+#	loaded as coords with pymol. In a terminal (some linux distro):
+#	pymol my_coords.xyz
+#	Once in pymol, enter get_view on the command line e copy the result.
+#	You should format your set of values as presented on "the_view" 
+#	variable.
+
+the_view =    ( 0.414068997,    0.909954250,    0.023042712,
+				0.329461694,   -0.173420861,    0.928105175,
+				0.848530829,   -0.376706958,   -0.371601909,
+				0.000000000,    0.000000000,  -43.606266022,
+				1.609671593,   -0.394682407,   -0.436405420,
+				39.733062744,   47.479469299,  -20.000000000)
 
 # workspace settings
 
@@ -77,59 +89,65 @@ cmd.set("dash_radius",0.035)
 cmd.distance('coordinate','elem Eu', 'elem N', 2.7,label=0)
 cmd.distance('coordinate','elem Eu', 'elem O', 2.7,label=0)
 
+def create_png(coords,cube=None,labelname=None,colorA='red',colorB='blue'):
+	"""
+	This is a simple function to create nice png images from a xyz file
+	and a set of cube files. If only the xyz file is passed, this 
+	function will return a png image containing the molecular geometry 
+	stored in it using PyMol's cmd. You should provide set_view, 
+	open pymol and load the xyz file, type get_view at the prompt and 
+	copy the values to "cmd.set_view". If the script find some cube 
+	files on the work directory, it will try to render isosurface's 
+	images. You may wish to change the isosurface's color, this can be 
+	done by altering the values of colorA and colorB parameters. 
+	"""
 
-# Start a rudimentary for loop that iterates over all the files 
-# inside file_om and get the regex inside .mo****a. this is a far 
-# more soft solution than try to slice each string in file_om 
-# with the file_om[i][:] method 
-
-for cube in file_om:
-	temp = re.search('(?<=mo)[0-9]+',cube)
-	labelname = temp.group(0)
-
-#	This section performs a small iteration over the list that stores the .cube files 
-#	parsed by glob, for each cube file a sequency of PyMol's cmd calls are made, and 
-#	the loop will end generationg a nice png file
-
-for cube in file_om:
-	temp = re.search('(?<=mo)[0-9]+',cube)
-	labelname = temp.group(0)
 	cmd.load(coords,object='coords',format='xyz',quiet=1)
-
-#	Manuaally enter this array of values, you should open the same xyz file that is 
-#	loaded as coords with pymol. In a terminal (some linux distro):
-#	pymol my_coords.xyz
-#	Once in pymol, enter get_view on the command line e copy the result.
-	cmd.set_view((\
-				  0.414068997,    0.909954250,    0.023042712,\
-				  0.329461694,   -0.173420861,    0.928105175,\
-				  0.848530829,   -0.376706958,   -0.371601909,\
-				  0.000000000,    0.000000000,  -43.606266022,\
-				  1.609671593,   -0.394682407,   -0.436405420,\
-				  39.733062744,   47.479469299,  -20.000000000 ))
-  
-#	It is possible to set isovalues, colors and other stuffs
-#	the values you see are just the ones I used to prefer
-
-	cmd.load(cube,'Orb',format='cube',state=0)
-	cmd.isosurface('Asurf1','Orb', 0.03,state=0)
-	cmd.isosurface('Bsurf1','Orb', -0.03,state=0)
-	cmd.color(color="yellow",selection='Asurf1')
-	cmd.set('transparency',0.0,'Asurf1')
-	cmd.color(color="blue",selection='Bsurf1')
-	cmd.set('transparency',0.0,'Bsurf1')
+	cmd.set_view(the_view)
 	
-#	Label position is a headache, you should consider set the best 
-#	position for your visualization. GOOD LUCK!
+	#	It is possible to set isovalues, colors and other stuffs
+	#	the values you see are just the ones I used to prefer
+	if cube:	
+		cmd.load(cube,'Orb',format='cube',state=0)
+		cmd.isosurface('Asurf1','Orb', 0.03,state=0)
+		cmd.isosurface('Bsurf1','Orb', -0.03,state=0)
+		cmd.color(color=colorA,selection='Asurf1')
+		cmd.set('transparency',0.0,'Asurf1')
+		cmd.color(color=colorB,selection='Bsurf1')
+		cmd.set('transparency',0.0,'Bsurf1')
+		
+		#	Label position is a headache, you should consider set the best 
+		#	position for your visualization. GOOD LUCK!
+		
+		cmd.pseudoatom(object='foo',vdw=-1.0, hetatm=1,pos=[0,0,1.75])
+		cmd.show('spheres','foo')
+		cmd.label('foo',labelname)
+		cmd.set("label_size",-2)
+		cmd.set("label_outline_color","black")
+		cmd.set("label_color","white")
+		cmd.set("label_font_id",7)
+		cmd.set("label_position",[-6,7,1.75])
+		cmd.png(labelname,width=1320,height=1160,ray=1)
+		cmd.delete('Orb') 
+		if cube:
+			cmd.delete('Orb')
+			cmd.delete('Asurf1')
+			cmd.delete('Bsurf1')
+			cmd.delete('foo')
+			cmd.png('coords',width=1320,height=1160,ray=1)	
+	else:
+		cmd.png('coords',width=1320,height=1160,ray=1)
+	return 
 
-	cmd.pseudoatom(object='foo',pos=[0,-9,1.75])
-	cmd.show('spheres','foo')
-	cmd.label('foo',labelname)
-	cmd.set("label_size",-2)
-	cmd.set("label_outline_color","black")
-	cmd.set("label_color","black")
-	cmd.set("label_font_id",10)
-	cmd.set("label_position",[-3,4,1.75])
-	cmd.png(labelname,width=660,height=580,ray=1)
-	cmd.delete('Orb') 
-
+if not file_om:
+	print("Nothing to do about surfaces here!\nI can generate a nice geom for you instead.")
+	setview = the_view
+	create_png(coords,the_view)
+	
+else:
+	print("Let's get some plots!\n But I'll give you a nice\n geom picture.\n Consider this as a BONUS.")	
+	for cube in file_om:
+		temp = re.search('(?<=mo)[0-9]+',cube)
+		labelname = temp.group(0)
+		create_png(coords,cube,labelname)
+		
